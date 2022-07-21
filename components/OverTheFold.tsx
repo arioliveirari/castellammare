11c42424a7aac95d143e48dc394ee325abd2750a
unknown <joaquincerruti54@gmail.com>
2022-07-21 10:51:51 -0300

    heade & over the fold created

--- components/OverTheFold.tsx
+++ components/OverTheFold.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
-import { usc, usb } from '../utils/helpers';
+import { usc, usb, usw } from '../utils/helpers';
 import styles from '../styles/overTheFold.module.scss';
 
-const OverTheFold = ({ children }: any) =>  (
-  <div className={usc(styles,['OverTheFold'])}>
-    <div className={usb(['contianer'])}>
-      <p>Content! </p>
+const OverTheFold = ({ children }: any) => (
+  <div className={usc(styles, ['OverTheFold'])}>
+    <div className={usb(['container-fluid', 'm-auto'])}>
+      <div className={usc(styles, ['conteinerOver'])}>
+        <div className={usc(styles, ["textOver"])}>
+          <span className={usc(styles, ["tittleOver"])}>
+            Baires Navega
+          </span>
+          <div className={usc(styles, ["paragraphOver"])}>
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+              Lorem Ipsum has been the industry's standard dummy text ever  since the 1500s,
+              when an unknown printer took
+            </p>
+          </div>
+          <a href='#' className={usc(styles, ["buttonOver"])} >Reserva tu lugar</a>
+        </div>
+      </div>
     </div>
   </div>
 );
--- components/globals/Header.tsx
+++ components/globals/Header.tsx
@@ -1,15 +1,47 @@
 import React from 'react';
 import Head from 'next/head';
-import { usc } from '../../utils/helpers';
+import { usb, usc } from '../../utils/helpers';
+<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
 import styles from '../../styles/headers.module.scss';
 
-const Header = ({ children }: any) =>  (
-  <div className={usc(styles,['Header'])}>
+const Header = ({ children }: any) => (
+  <div className={usc(styles, ['Header'])}>
     <Head>
       <title>Baires navega!</title>
       <meta name="description" content="Baires navega" />
       <link rel="icon" href="/favicon.ico" />
+      <script src="https://kit.fontawesome.com/78b8c96f09.js" crossOrigin="anonymous"></script>
     </Head>
+    <p className={usc(styles, ["logo"])}>
+      Baires Navega
+    </p>
+    <div className={usc(styles, ["links"])}>
+      <div className={usc(styles, ["contentLinks"])}>
+        <a href="#">Sobre nosotros</a>
+        <a href="#">Servicios</a>
+        <a href="#">Reservar</a>
+      </div>
+    </div>
+
+    <div className={usc(styles, ["icons"])}>
+      <div>
+        <a href="#">
+          {/* <i className={usc(styles, ["fa-brands fa-whatsapp"])}>
+        </i> */}
+          whatsapp
+        </a>
+        <a href="#">
+          {/* <i className={usc(styles, ["fa-brands fa-instagram"])}>
+        </i> */}
+          instagram
+        </a>
+        <a href="#">
+          {/* <i className={usc(styles, ["fa-brands fa-envelope"])}>
+        </i> */}
+          email
+        </a>
+      </div>
+    </div>
   </div>
 );
 
