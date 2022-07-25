aa7941eb31153f6e39d1b1ad214a24fe28d8cae9
unknown <joaquincerruti54@gmail.com>
2022-07-25 09:13:46 -0300

    styled form

--- components/Form.tsx
+++ components/Form.tsx
@@ -6,21 +6,39 @@ const Form = ({ children }: any) => (
     <div className={usw(styles, ['formComponent'], ["container-fluid"])}>
         <div className={usb(['container'])}>
             <div className={usb(["row"])}>
-                <div className={usb(["col-md-10", "col-12", 'm-auto'])}>
+                <div className={usb(["col-md-10", "col-12"])}>
                     <div className={usc(styles, ["content"])}>
-                        <div className={usc(styles, ["left"])}>
-                            <div className={usc(styles, ["kiwi"])}>
-                                kiwi
-                            </div>
-                            <div className={usc(styles, ["mango"])}>
-                                mango
-                            </div>
+                        <div className={usc(styles, ["picContainer"])} >
+                            <div className={usc(styles, ["kiwi"])}></div>
+                            <div className={usc(styles, ["mango"])}></div>
                         </div>
-                        <div className={usc(styles, ["rigth"])} >
-                            <div className={usc(styles,["form"])}>
+
+
+                        <div className={usc(styles, ["form"])}>
+                            <div className={usc(styles, ["formText"])}>
+                                <div className={usc(styles, ["formTittle"])} >
+                                    vivi una experiencia
+                                </div>
+                                <div className={usc(styles, ["formParagraph"])} >
+                                    unica
+                                </div>
 
                             </div>
+                            <div className={usc(styles, ["formInput"])} >
+                                aaa
+                            </div>
+                            <div className={usc(styles, ["formInput"])} >
+                                aaa
+                            </div>
+                            <div className={usc(styles, ["formInput"])} >
+                                aaa
+                            </div>
+                            <p>
+                                Lorem Ipsum dolor sit amet consecteur?
+                            </p>
+
                         </div>
+
                     </div>
                 </div>
             </div>
