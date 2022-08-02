f65eefe753ecde0b4c3274aaefe500df6de2c79f
unknown <joaquincerruti54@gmail.com>
2022-08-02 10:56:09 -0300

    footer section detailed, captains section initialized

---
+++ components/captains.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { usc, usb, usw } from '../utils/helpers';
+import styles from '../styles/captains.module.scss';
+
+const Captains = () =>{
+    return(
+        <div>
+            <p>
+                hola mundo
+            </p>
+        </div>
+    )
+}
